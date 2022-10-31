0fe1487f441d17ab3afb4e20447be22ed79eda3f
Vasyl Polishchuk <vasylpolishchuk22@gmail.com>
2022-10-31 12:57:02 +0200

    made sticky nav

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
 const navOpen = document.querySelector('.nav-open');
 const navClose = document.querySelector('.nav-close');
 const nav = document.querySelector('.primary-nav');
+const header = document.querySelector('.primary-header')
 
 navOpen.addEventListener('click', () => {
     nav.setAttribute('data-visible', true);
 })
 navClose.addEventListener('click', () => {
     nav.setAttribute('data-visible', false);
+})
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > header.offsetTop) {
+        document.body.style.paddingTop = `${header.offsetHeight}px`
+        // nav.classList.add('sticky')
+        header.classList.add('sticky')
+    } else {
+        document.body.style.paddingTop = '0'
+        // nav.classList.remove('sticky')
+        header.classList.remove('sticky')
+    }
 })
